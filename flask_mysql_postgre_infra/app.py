310a0ff4af52b8e3ce245e575057f415315875ae
yennanliu <f339339@gmail.com>
2019-01-28 16:55:30 +0000

    add a flask app run OP on mysql/postgresql

---
+++ flask_mysql_postgre_infra/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, make_response, g
+import os
+import random
+import json
+import mysql
+import 
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return 'hello-world'
+
+@app.route("/mysql_test", methods=['POST','GET'])
+def get_mysql_test():
+    config = {
+        'user': 'root',
+        'password': 'password',
+        'host': 'db',
+        'port': '3306',
+        'database': 'knights'}
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM favorite_colors')
+    results = [{name: color} for (name, color) in cursor]
+    cursor.close()
+    connection.close()
+    return results
+
+
+@app.route("/postgre_test", methods=['POST','GET'])
+def get_postgre_test():
+    pass 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+
+
+
