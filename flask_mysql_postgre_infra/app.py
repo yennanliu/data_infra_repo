e88732466a2b1ba5aa7ee9d4354ad9d0efed6e9c
yennanliu <f339339@gmail.com>
2019-01-31 15:21:38 +0000

    fix postgre connection and add new func can connect to both mysql and postgre

--- flask_mysql_postgre_infra/app.py
+++ flask_mysql_postgre_infra/app.py
@@ -4,6 +4,7 @@ import random
 import json
 import pymysql
 import pymysql.cursors
+import psycopg2
 
 app = Flask(__name__)
 
@@ -22,6 +23,39 @@ def get_mysql_data():
     cursor.close()
     connection.close()
     return result[0]
+
+def get_postgre_data():
+    conn = psycopg2.connect("dbname='knights' user='postgres' host='postgres' password='password'")
+    with conn.cursor()  as cursor:
+        sql = """SELECT * FROM favorite_colors"""
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print (result)
+    cursor.close()
+    conn.close()
+    return result[0]
+
+def get_db_data(dbtype):
+    if dbtype == 'mysql':
+        connection = pymysql.connect(host='mysql',
+                             user='root',
+                             password='password',
+                             db='knights',
+                             cursorclass=pymysql.cursors.DictCursor)
+    elif dbtype == 'postgres':
+        connection = psycopg2.connect("dbname='knights' user='postgres' host='postgres' password='password'")
+    else:
+        print ('not support DB type')
+        pass 
+    with connection.cursor() as cursor:
+        sql = """SELECT * FROM favorite_colors"""
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print (result)
+    cursor.close()
+    connection.close()
+    return result[0]
+
 #  OP -----------------------------------------
 
 
@@ -31,11 +65,11 @@ def hello():
 
 @app.route("/mysql_test")
 def get_mysql_test():
-    return json.dumps({'favorite_colors': get_mysql_data()})
+    return json.dumps({'favorite_colors': get_db_data('mysql')})
 
 @app.route("/postgre_test", methods=['POST','GET'])
 def get_postgre_test():
-    pass 
+    return json.dumps({'favorite_colors': get_db_data('postgres')}) 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
