7f722fd39aac76beab2dbebe417acf40cc0918b4
yennanliue <f339339@gmail.com>
2019-02-02 11:45:18 +0000

    backup func kill VA dockers with env var

--- kill_clean_docker_image_instance.sh
+++ kill_clean_docker_image_instance.sh
@@ -12,6 +12,28 @@ function kill_all_docker_image_instance(){
 }
 
 
+function kill_VA_docker_image_instance(){
+    # https://github.com/astronomer/astronomer/blob/master/bin/build-airflow
+	VA_docker_container=`docker ps --format '{{.Names}}' | grep "${va_docker_container_name}"`
+	VA_docker_image=`docker images | grep -E '${va_docker_image_name}' | awk '{print $1}'`
+	VA_docker_image_id=`docker images | grep -E '${va_docker_image_name}' | awk '{print $3}'`
+
+	echo 'STOP --------------------------'
+	for container in ${VA_docker_container}; 
+	do
+		echo "kill docker container $container" &&  docker stop $container; 
+	done; 
+
+	echo 'REMOVE --------------------------'
+	for image_id in ${VA_docker_image_id}; 
+	do 
+		echo "docker rm image_id $image_id" && docker rm  $image_id; 
+		echo "docker rmi image_id $image_id" && docker rmi  $image_id ; 
+	done; 
+
+}
+
+
 function kill_xbot_airflow_docker_image_instance(){
 
 	all_docker_container=`docker ps --format '{{.Names}}'`
@@ -34,5 +56,11 @@ function kill_xbot_airflow_docker_image_instance(){
 
 }
 
+
+# kill xbot astro dockers 
 kill_xbot_airflow_docker_image_instance
 
+
+# kill VA dockers 
+#for $va_docker_image_name in ${VA_DOCKER_IMAGES_NAME} ; do
+#    kill_VA_docker_image_instance  "${va_docker_image_name}"
