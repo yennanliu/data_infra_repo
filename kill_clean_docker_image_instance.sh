74c3aab99838fb9aadbff91d91e74b22af346873
yennanliue <f339339@gmail.com>
2019-02-02 11:35:27 +0000

    fix remove docker images via docker image id

--- kill_clean_docker_image_instance.sh
+++ kill_clean_docker_image_instance.sh
@@ -17,18 +17,19 @@ function kill_xbot_airflow_docker_image_instance(){
 	all_docker_container=`docker ps --format '{{.Names}}'`
 	xbot_docker_container=`docker ps --format '{{.Names}}' | grep "xbot"`
 	xbot_docker_image=`docker images | grep -E 'postgres|x-bot/airflow|astronomerinc/ap-airflow' | awk '{print $1}'`
+	xbot_docker_image_id=`docker images | grep -E 'postgres|x-bot/airflow|astronomerinc/ap-airflow' | awk '{print $3}'`
 
 	echo 'STOP --------------------------'
 	for container in ${xbot_docker_container}; 
 	do
 		echo "kill docker container $container" &&  docker stop $container; 
 	done; 
-	
+
 	echo 'REMOVE --------------------------'
-	for image in ${xbot_docker_image}; 
+	for image_id in ${xbot_docker_image_id}; 
 	do 
-		echo "docker rm image $image" && docker rm  $image; 
-		echo "docker rmi image $image" && docker rmi  $image ; 
+		echo "docker rm image_id $image_id" && docker rm  $image_id; 
+		echo "docker rmi image_id $image_id" && docker rmi  $image_id ; 
 	done; 
 
 }
