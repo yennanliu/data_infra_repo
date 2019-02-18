5160da835251dbdaf6e6d0a54e78a77e8ccafa6a
yennanliue <f339339@gmail.com>
2019-02-18 08:50:56 +0000

    add quick start via docker compose

--- flask_mysql_postgre_infra/build_infra.sh
+++ flask_mysql_postgre_infra/build_infra.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+
+
+# METHOD 1) : DOCKER COMPOSE -------------
+cd ~ && data_infra_repo/flask_mysql_postgre_infra
+docker-compose  -f docker-compose.yml up 
+
+
+
+# METHOD 2) : DOCKER RUN -------------
 # https://hub.docker.com/_/postgres
 # STEP 1) start a postgres instance
 docker run --name some-postgres -e POSTGRES_PASSWORD=password -d postgres
