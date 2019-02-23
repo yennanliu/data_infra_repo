4447d37e1688ae59572fa7f1b2e717750e4f2bb0
yennanliue <f339339@gmail.com>
2019-02-23 09:24:02 +0000

    update command built infra

--- celery_redis_flower_infra/build_infra.sh
+++ celery_redis_flower_infra/build_infra.sh
@@ -6,8 +6,18 @@
 # SCRIPT HELP BUILD INFRA LOCAL 
 #################################################################
 
-cd ~ && cd data_infra_repo/celery_rabbitmq_cluster_infra
-docker pull rabbitmq:latest
-docker-compose build
-docker-compose scale worker=5
-docker-compose up
+cd ~ && cd data_infra_repo/celery_redis_flower_infra
+docker-compose -f  docker-compose.yml up 
+
+# to add more workers 
+docker-compose up -d --scale worker=5 --no-recreate
+
+# to shut down
+docker-compose down
+
+
+# access the services 
+#Check the flower UI : http://localhost:5555/
+# Run a "add" task : http://localhost:5001/add/1/2
+# Run a "mul" task : http://localhost:5001/add/1/2
+# Run a "web scrape" task : http://localhost:5001/scrap_task
