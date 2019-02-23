cdb48cc383fb2ec96881e9f526f70a32163c2cde
yennanliue <f339339@gmail.com>
2019-02-23 07:40:13 +0000

    import mul, web_scrap tasks into tasks.py

--- celery_redis_flower_infra/celery-queue/tasks.py
+++ celery_redis_flower_infra/celery-queue/tasks.py
@@ -11,5 +11,19 @@ celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND
 
 @celery.task(name='tasks.add')
 def add(x: int, y: int) -> int:
-    time.sleep(5)
-    return x + y
+	time.sleep(5)
+	return x + y
+
+@celery.task(name='tasks.add')
+def mul(x: int, y: int) -> int:
+	time.sleep(5)
+	return x*y
+
+@celery.task(name='tasks.scrap_task')
+def scrape():
+	url = 'https://github.com/apache/spark'
+	opener=urllib.request.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	page = opener.open(url)
+	soup = BeautifulSoup(page)
+	print (soup.text)
