f2835672fee39ede27ce11195e982beab837f2df
yennanliue <f339339@gmail.com>
2019-03-21 00:54:03 +0000

    add travis help build bash

---
+++ travis_build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Prerequisite
+# Make sure you set secret enviroment variables in Travis CI
+# DOCKER_USERNAME
+# DOCKER_PASSWORD
+
+# https://github.com/alpine-docker/git/blob/master/build.sh
+
+set -e
+
+image="alpine/git"
+
+tag=`git tag -l --points-at HEAD`
+
+if [[ "$TRAVIS_BRANCH" == "master" && "${tag}" =~ ^1.0 ]]; then
+  docker build --no-cache -t ${image}:${tag} .
+  docker tag ${image}:${tag} ${image}:latest
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker push ${image}:${tag}
+  docker push ${image}:latest
+fi
