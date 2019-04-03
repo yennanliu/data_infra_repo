501bca2868d3eea615b2b77d954e00d77dc672b0
yennanliue <f339339@gmail.com>
2019-04-03 09:55:26 +0100

    fix docker run command (run command outside of docker via CLI)

--- build.sh
+++ build.sh
@@ -6,16 +6,14 @@
 
 echo ' ---------------- BUILD ALL REPO DOCKER IMAGES ----------------'
 
-echo REGISTRY_USER = $REGISTRY_USER && echo REGISTRY_PASS = $REGISTRY_PASS
-
 declare -a docker_images=("celery_redis_flower_infra/."  "flask_mysql_postgre_infra/.")
 
 for docker_images in "${docker_images[@]}"
 	do 
 		instance_name="$(cut -d'/' -f1 <<<"$docker_images")"
 		# docker build 
-		echo 'docker bulid : $docker_images .... ' && docker build $docker_images -t $instance_name	
+		echo 'docker bulid : ' $docker_images  && docker build $docker_images -t $instance_name	
 		# run test 
-		docker run -it $instance_name 'docker test 123'	
+		docker run -it $instance_name  echo 'docker test 123'	
 		#echo $docker_images $instance_name
 	done 
