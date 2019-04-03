49de8476e93da66f9f242b31beee9e4b3d480c2b
yennanliue <f339339@gmail.com>
2019-04-03 09:26:31 +0100

    loop build all docker images in the repo

--- build.sh
+++ build.sh
@@ -4,4 +4,14 @@
 # SHELL SCRIPT BUILD DOCKER INSTANCE FOR TRAVIS CI   
 #################################################################
 
-# dev 
+echo ' ---------------- BUILD ALL REPO DOCKER IMAGES ----------------'
+echo REGISTRY_USER = $REGISTRY_USER && echo REGISTRY_PASS = $REGISTRY_PASS
+
+declare -a docker_images=("celery_redis_flower_infra/."  "flask_mysql_postgre_infra/.")
+for docker_images in "${docker_images[@]}"
+	do 
+		echo 'docker bulid : $docker_images .... ' && docker build $docker_images
+		#echo $docker_images
+	done 
+# run test 
+#docker run -it Xbot_env_instance echo 'docker test 123'
