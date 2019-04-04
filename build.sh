03542e3af820aaf848915b79212d79cdc761c639
yennanliue <f339339@gmail.com>
2019-04-04 07:50:46 +0100

    fix docker_image var name in shell script

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ echo ' ---------------- BUILD ALL REPO DOCKER IMAGES ----------------'
 
 declare -a docker_images=("celery_redis_flower_infra/."  "flask_mysql_postgre_infra/.")
 
-for docker_images in "${docker_images[@]}"
+for docker_image in "${docker_images[@]}"
 	do 
 		instance_name="$(cut -d'/' -f1 <<<"$docker_images")"
 		# docker build 
-		echo 'docker bulid : ' $docker_images  && docker build $docker_images -t $instance_name	
+		echo 'docker bulid : ' $docker_image  && docker build $docker_image -t $instance_name	
 		# run test 
 		docker run -it $instance_name  echo 'docker test 123'	
 	done 
