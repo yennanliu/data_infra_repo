3103da8ef45de65a7b3f5187434409849c078a69
yennanliu <f339339@gmail.com>
2019-06-28 13:50:22 +0800

    add mlflow-infra, zeepelin_elasticsearch_infra to to-build-docker list

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@
 
 echo ' ---------------- BUILD ALL REPO DOCKER IMAGES ----------------'
 
-declare -a docker_images=("celery_redis_flower_infra/."  "flask_mysql_postgre_infra/.")
+declare -a docker_images=("celery_redis_flower_infra/."  "flask_mysql_postgre_infra/." "mlflow_infra/." "zeepelin_elasticsearch_infra/.")
 
 for docker_image in "${docker_images[@]}"
-	do 
-		instance_name="$(cut -d'/' -f1 <<<"$docker_images")"
-		# docker build 
-		echo 'docker bulid : ' $docker_image  && docker build $docker_image -t $instance_name	
-		# run test 
-		docker run -it $instance_name  echo 'docker test 123'	
-	done 
+ do 
+  instance_name="$(cut -d'/' -f1 <<<"$docker_images")"
+  # docker build 
+  echo 'docker bulid : ' $docker_image  && docker build $docker_image -t $instance_name	
+  # run test 
+  docker run -it $instance_name  echo 'docker test 123'	
+ done 
