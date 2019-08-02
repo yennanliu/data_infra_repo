c926c345fd04636ffabeb4ac05655004090d0e08
Travis CI <f339339@gmail.com>
2019-08-03 00:19:51 +0800

    move db init script, sql to /sql_script for run them right after mysql built

---
+++ mysql-multiple-table/sql_script/init_db.sh
@@ -0,0 +1,7 @@
+# #!/bin/bash
+# /usr/bin/mysqld_safe --skip-grant-tables &
+# sleep 5
+# echo "CREATE DATABASE my_db..."
+# mysql -u root -e "CREATE DATABASE my_db"
+# echo "Insert data into my_db..."
+# mysql -u root my_db < dump_data.sql
