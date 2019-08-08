042b2af0917cf9ec6c29fbe2f3c66fda08f04aa2
Travis CI <f339339@gmail.com>
2019-08-08 22:51:47 +0800

    fix to run sql command via docker, but not mysql client

--- mysql-multiple-table/db_analysis.sh
+++ mysql-multiple-table/db_analysis.sh
@@ -4,30 +4,28 @@
 # SCRIPT DOES MYSQL DATA ANALYSIS 
 #################################################################
 
-mysql -u root <<EOF
+docker exec -it mysql_env /usr/bin/mysql -u root  -e  "
 SHOW DATABASES;
 use my_db ; 
 SHOW TABLES; 
-SELECT 'RUN THE ANALYSIS PROCESS..'; 
-EOF
+SELECT 'RUN THE ANALYSIS PROCESS..';" 
 
 # query1 
-mysql -u root <<EOF
+docker exec -it mysql_env /usr/bin/mysql -u root  -e  "
 use my_db ; 
 SELECT
 Client_Id,
 COUNT(*) AS ORDER_PER_USER 
 FROM 
 Trips
-GROUP BY 1 ORDER BY 1; 
-EOF
+GROUP BY 1 ORDER BY 1;" 
 
 # query2 
-mysql -u root <<EOF
+docker exec -it mysql_env /usr/bin/mysql -u root  -e  "
+use my_db ; 
 SELECT t.* FROM 
 Trips t
 JOIN Users u  
 ON 
 t.Client_Id = u.Users_Id
-WHERE u.Banned = 'No'; 
-EOF
+WHERE u.Banned = 'No';"
