19a1d5b9f1ba5f4528fc7a26027f6e718129427d
yennanliu <f339339@gmail.com>
2019-08-08 18:40:44 +0800

    add script can init db by run mysql container and clean no need code

--- mysql-multiple-table/sql_script/init_db.sh
+++
@@ -1,7 +0,0 @@
-# #!/bin/bash
-# /usr/bin/mysqld_safe --skip-grant-tables &
-# sleep 5
-# echo "CREATE DATABASE my_db..."
-# mysql -u root -e "CREATE DATABASE my_db"
-# echo "Insert data into my_db..."
-# mysql -u root my_db < dump_data.sql
