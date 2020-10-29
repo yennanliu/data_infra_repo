cb11ea76fac5ed936099b2512292716cbe2383e3
yennanliu <f339339@gmail.com>
2020-10-29 22:30:02 +0800

    add files

--- hadoop_yarn_spark/README.md
+++ hadoop_yarn_spark/README.md
@@ -3,6 +3,9 @@
 git clone https://github.com/yennanliu/data_infra_repo.git
 cd data_infra_repo/hadoop_yarn_spark
 docker-compose up -d
+
+# down the service 
+docker-compose down
 ```
 ### Ref
 - https://www.jianshu.com/p/3ca4c759d3d8
---
+++ hadoop_yarn_spark/src/main/scala/rdd/WordCount.scala
@@ -0,0 +1,29 @@
+package rdd
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.{SparkConf, SparkContext}
+
+/*
+Simple word count example running local 
+ */
+
+object WordCount extends App{
+  val config = ConfigFactory.load()
+  // modify file_path from config.getString to hardcode here 
+  //val filePath = config.getString("file_path")
+  val filePath = "src/main/resources/sample.txt"
+  val sparkMaster = config.getString("SPARK_MASTER")
+  val sparkAppName = config.getString("SPARK_APPNAME")
+
+  val conf = new SparkConf().setMaster(sparkMaster).setAppName(sparkAppName)
+  val sc = new SparkContext(conf)
+  sc.setLogLevel("ERROR")
+
+  val lines = sc.textFile(filePath)
+  val words = lines.flatMap(line => line.split(" "))
+  val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
+
+  println("foreach over collect will output in same order of textfile")
+  counts.collect().foreach(println)
+
+  sc.stop()
+}
